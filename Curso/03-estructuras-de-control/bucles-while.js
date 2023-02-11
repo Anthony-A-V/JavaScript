58965d1086d4235ad36632c0538f8c214bc6e108
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-02-11 15:57:43 -0500

    cambios

---
+++ Curso/03-estructuras-de-control/bucles-while.js
@@ -0,0 +1,16 @@
+// Bucle While
+
+let i = 0;
+let max = 10;
+while (i < max) {
+  console.log(i);
+  i++;
+}
+
+i = 15;
+
+// Bucle Do...While
+
+do {
+  console.log("Estoy en el do while");
+} while (i < max);
