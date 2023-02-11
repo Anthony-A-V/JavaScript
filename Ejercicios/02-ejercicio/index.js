0190928cad6939ae3f804433e7b61b358cec2e27
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-02-11 11:15:20 -0500

    cambios

--- Ejercicios/02-ejercicio/index.js
+++ Ejercicios/02-ejercicio/index.js
@@ -1,10 +1,9 @@
 const libro = {
-    titulo: "La ciudad y los perros",
-    autor: "Mario Vargas Llosa",
-    fecha: "1963",
-    url: "https://es.wikipedia.org/wiki/La_ciudad_y_los_perros"
+  titulo: "La ciudad y los perros",
+  autor: "Mario Vargas Llosa",
+  fecha: "1963",
+  url: "https://es.wikipedia.org/wiki/La_ciudad_y_los_perros",
+};
 
-}
-
-const lista = ["Anthony", 25, true, new Date ("Aug 05 1997"), libro]
+const lista = ["Anthony", 25, true, new Date("Aug 05 1997"), libro];
 console.log(lista);
