46d98fbdcf7f0ab9cc081ed72c8148184ad64017
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-02-10 21:23:27 -0500

    first commit

---
+++ Curso/01-02-mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,12 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*
+Esta es la parte intermedia de nuestro proyecto
+En esta parte realizamos los chequeos necesarios
+Por último vamos a la última línea de saludo 
+Aquí también
+*/
+
+// Este va a ser el mensaje que se envía para despedir
+console.log("Adios");
