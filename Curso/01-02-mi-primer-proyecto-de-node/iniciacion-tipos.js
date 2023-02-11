b798a01c349af9d20d4ac3990e1bc23a2398f4ea
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-02-11 11:14:01 -0500

    cambios

---
+++ Curso/01-02-mi-primer-proyecto-de-node/iniciacion-tipos.js
@@ -0,0 +1,25 @@
+// Tipos primitivos
+
+// number
+4;
+0;
+
+// string
+"Hola Mundo";
+"Hola Mundo";
+`Hola Mundo`;
+
+// booleanos
+true;
+false;
+
+// nulo y undefined
+null;
+undefined;
+
+// null, undefined, false, 0 ==> Todas son Falsy
+if (true) {
+  console.log("cumple");
+} else {
+  console.log("no cumple");
+}
