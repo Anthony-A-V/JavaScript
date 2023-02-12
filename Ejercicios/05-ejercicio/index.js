13ec0fb15881e6d303ae26571f886165d8670b1d
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-02-11 21:08:09 -0500

    cambios

--- Ejercicios/05-ejercicio/index.js
+++ Ejercicios/05-ejercicio/index.js
@@ -3,12 +3,11 @@ let alturaMetros = 1.85;
 let pesoKilogramos = 70.9;
 let alturaRedondeada = Math.ceil(alturaMetros);
 let pesoRedondeado = Math.floor(pesoKilogramos);
-let variable =
-  "el máximo valor que se puede obtener en Javascript + 1" == Number.MAX_VALUE;
+let sonIguales = Number.MAX_VALUE + 1 == Number.MAX_VALUE;
 
 console.log(alturaCentimetros);
 console.log(alturaMetros);
 console.log(pesoKilogramos);
 console.log(alturaRedondeada);
 console.log(pesoRedondeado);
-console.log(variable);
+console.log(sonIguales);
