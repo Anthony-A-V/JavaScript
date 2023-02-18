a98f56a3e7e4acab457658654b30e4110d53de7e
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-02-17 21:04:12 -0500

    cambios

--- Ejercicios/15-ejercicio/index.js
+++ Ejercicios/15-ejercicio/index.js
@@ -1,12 +1,15 @@
 const nombre = "Anthony";
 const apellido = "Anguiz";
 const datos = {
-  nombre: nombre,
-  apellido: apellido
+  nombre,
+  apellido
 };
 
 sessionStorage.setItem("datos-sesion", JSON.stringify(datos));
 localStorage.setItem("datos-sesion", JSON.stringify(datos));
 
-document.cookie = "objetoCookie=" + JSON.stringify(datos);
-+"; expires=" + new Date(2023, 0, 1, 2, 52, 59, 100).toUTCString();
+document.cookie =
+  "objetoCookie=" +
+  JSON.stringify(datos) +
+  "; expires=" +
+  new Date(2023, 1, 17, 21, 5, 0, 100).toUTCString();
