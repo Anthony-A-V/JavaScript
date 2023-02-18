088464aa925e60943da68d63793e7a033773398e
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-02-17 22:18:51 -0500

    cambios

---
+++ Ejercicios/16-ejercicio/index.js
@@ -0,0 +1,47 @@
+const parrafos = document.querySelectorAll(".parrafo");
+const secciones = document.querySelectorAll(".seccion");
+const borrar = document.querySelector(".borrar");
+
+parrafos.forEach((parrafo) => {
+  parrafo.addEventListener("dragstart", (event) => {
+    console.log("Estoy arrastrando el párrafo: " + parrafo.innerText);
+    parrafo.classList.add("dragging");
+    event.dataTransfer.setData("id", parrafo.id);
+    const elemento_fantasma = document.querySelector(".imagen-fantasma");
+    event.dataTransfer.setDragImage(elemento_fantasma, 0, 0);
+  });
+
+  parrafo.addEventListener("dragend", () => {
+    console.log("He terminado de arrastrar");
+    parrafo.classList.remove("dragging");
+  });
+});
+
+secciones.forEach((seccion) => {
+  seccion.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move"; // copy por defecto
+    // console.log("Dreag Over");
+  });
+
+  seccion.addEventListener("drop", (event) => {
+    console.log("Drop");
+    const id_parrafo = event.dataTransfer.getData("id");
+    // console.log("Párrafo id: ", id_parrafo);
+    const parrafo = document.getElementById(id_parrafo);
+    seccion.appendChild(parrafo);
+  });
+});
+
+borrar.addEventListener("dragover", (event) => {
+  event.preventDefault;
+  event.dataTransfer.dropEffect = "move";
+});
+
+borrar.addEventListener("drop", (event) => {
+  console.log("Drop en borrar");
+  const id_parrafo = event.dataTransfer.getData("id");
+  // console.log("Párrafo id: ", id_parrafo);
+  const parrafo = document.getElementById(id_parrafo);
+  borrar.appendChild(parrafo);
+});
